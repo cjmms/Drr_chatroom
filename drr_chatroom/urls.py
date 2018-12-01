1c2cc3bba88ed187d2a88ce865f2d9998a820507
LuckyDog <lucky061681@gmail.com>
2018-12-01 18:47:28 -0500

    customize login page

--- drr_chatroom/urls.py
+++ drr_chatroom/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include('django.contrib.auth.urls')),
+    path('test/',TemplateView.as_view(template_name='index.html'), name='home').
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
