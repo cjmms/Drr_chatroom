2575854c506f8f8bc8c180c217632b8b8ca91ee5
LuckyDog <lucky061681@gmail.com>
2018-12-01 19:04:01 -0500

    delete custom login page

--- drr_chatroom/urls.py
+++ drr_chatroom/urls.py
@@ -20,6 +20,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include('django.contrib.auth.urls')),
-    path('test/',TemplateView.as_view(template_name='index.html'), name='home').
+    path('test/',TemplateView.as_view(template_name='index.html'), name='test'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
