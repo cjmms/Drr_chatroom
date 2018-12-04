2b3ece29fc0957944ba737cc050dddfcb28080be
LuckyDog <lucky061681@gmail.com>
2018-12-04 03:07:57 -0500

    maintain login page

--- drr_chatroom/urls.py
+++ drr_chatroom/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', include('signup.urls')),
     path('index/', include('django.contrib.auth.urls')),
-    #path('test/',TemplateView.as_view(template_name='index.html'), name='test'),
+    path('test/',TemplateView.as_view(template_name='index.html'), name='test'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('chat/', include('chatroom.urls')),
 ]
